b3ad71474e3757706aaa14c6fd94022578559da9
Kapil Kumar <kapil.kumar@rtctek.global>
2026-01-28 13:46:50 +0530

    change vitereact and create react

--- 1vitereact/src/App.jsx
+++ 1vitereact/src/App.jsx
@@ -3,10 +3,13 @@ import Chai from "./chai"
 function App() {
   // const [count, setCount] = useState(0)
 
+  const username = "chai aur code" ;
+
+
   return (
     <>
     <Chai />
-    <h1>Chai aur react with vite | Isskander Chauhan</h1>
+    <h1>Chai aur react with vite | Isskander Chauhan {username}</h1>
 
     </>
    
--- 1vitereact/src/main.jsx
+++ 1vitereact/src/main.jsx
@@ -4,11 +4,44 @@ import React from 'react'
 import ReactDOM from 'react-dom/client' 
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App!12</h1>
+    </div>
+  )
+}
+// const ReactElement = {
+//   type: "a" ,
+//   props : {
+//     href : "https://google.com",
+//     target : "_blank"
+//   },
+//   children: "Click me to visit google" 
+// }
 
+const anotherElement = (
+  <a href="https://google.com" target="_blank">Visit Google</a>
+)
+
+const anotherUser = "chai aur react" ;
+
+
+const reactElement = React.createElement(
+  "a",
+  {href: "https://google.com",target: "_blank"},
+  "click me to visit Google again ",
+  anotherUser
+
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <App />
+    // <App />
+    // <MyApp/>
+    // anotherElement
+    reactElement
+    // MyApp() 
   // </React.StrictMode>,
 )
 
