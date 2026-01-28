9ee79d580de9bc7dfc6df8ce136e17f7b8452457
Kapil Kumar <kapil.kumar@rtctek.global>
2026-01-28 11:09:35 +0530

    added react and vite

---
+++ 1vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+import Chai from "./chai"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Chai />
+    <h1>Chai aur react with vite | Isskander Chauhan</h1>
+
+    </>
+   
+      
+  )
+}
+
+export default App
---
+++ 2_basic_react/src/App.js
@@ -0,0 +1,13 @@
+// import Chai from "./Chai"
+import Chai from "./Chai" ;
+
+function App() {
+  return (
+    <>   // this is known as to be fragment
+    <Chai />
+    
+    </>
+  );
+}
+
+export default App;
